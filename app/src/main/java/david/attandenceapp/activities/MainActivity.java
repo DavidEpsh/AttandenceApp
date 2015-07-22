737e4b9b2076ad6a3c37b88bff7a97186dcfb9c3
DavidEpsh <davidep1@gmail.com>
2015-07-22 16:20:45 +0300

    Activities

--- app/src/main/java/david/attandenceapp/activities/MainActivity.java
+++ app/src/main/java/david/attandenceapp/activities/MainActivity.java
@@ -1,20 +1,25 @@
 package david.attandenceapp.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import david.attandenceapp.R;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
