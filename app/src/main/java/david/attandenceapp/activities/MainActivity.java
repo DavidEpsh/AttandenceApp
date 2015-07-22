4f04f57b0b0c02d42bd8f3787b9c8d9ce97693bd
DavidEpsh <davidep1@gmail.com>
2015-07-22 14:01:55 +0300

    Activities

--- app/src/main/java/david/attandenceapp/activities/MainActivity.java
+++ app/src/main/java/david/attandenceapp/activities/MainActivity.java
@@ -1,10 +1,12 @@
-package david.attandenceapp;
+package david.attandenceapp.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import david.attandenceapp.R;
 
 
 public class MainActivity extends AppCompatActivity {
