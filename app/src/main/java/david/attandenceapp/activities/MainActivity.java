8a6876a24a2485a68385ea4d2e10cfea5ae7ac0a
DavidEpsh <davidep1@gmail.com>
2015-07-24 15:55:48 +0300

    all sttrings added

--- app/src/main/java/david/attandenceapp/activities/MainActivity.java
+++ app/src/main/java/david/attandenceapp/activities/MainActivity.java
@@ -1,17 +1,31 @@
 package david.attandenceapp.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import david.attandenceapp.R;
+import david.attandenceapp.models.DrawerItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private DrawerLayout Drawer;
+    private RecyclerView.Adapter mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private List<DrawerItem> navigationItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        navigationItemsList = new ArrayList<DrawerItem>();
+        addItemsToNavigationList();
     }
 
     @Override
@@ -42,5 +59,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addItemsToNavigationList(){
+        navigationItemsList.add(new DrawerItem(getString(R.string.attendance_list), R.drawable.ic_attendance));
+        navigationItemsList.add(new DrawerItem(getString(R.string.events_list), R.drawable.ic_add_event));
+        navigationItemsList.add(new DrawerItem(getString(R.string.add_attendant), R.drawable.ic_register));
+        navigationItemsList.add(new DrawerItem(getString(R.string.add_event), R.drawable.ic_add_event));
+        navigationItemsList.add(new DrawerItem(getString(R.string.settings), R.drawable.ic_settings));
     }
 }
